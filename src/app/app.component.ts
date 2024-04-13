1f43e6ffd940e7fcd5be3904a867373323b25247
Bavin kumar <bavinkumar599@gmail.com>
2024-04-14 01:15:08 +0530

    CHanges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,25 +13,40 @@ export class AppComponent {
   left = 0;
   xScale : number = 0;
   yScale: number = 0;
+  scalehelper: number = 0;
 
-  ngAfterViewInit(): void {
+ ngOnInit() : void {
     const style = document.getElementById('innerMainBody');
     console.log("Height : " + style.offsetHeight);
     console.log("width : " + style.offsetWidth);
     console.log("X width total : " + style.offsetWidth/1366);
     console.log("Y height total : " + style.offsetHeight/768);
-    setTimeout(() => {
-      this.xScale = style.offsetWidth/1600;
-      this.yScale = style.offsetHeight/900;
-    }, 0);
-    
-    fromEvent(window, "wheel").subscribe((ev: WheelEvent) => {
-      const newScale = this.scale - ev.deltaY * 0.2;
-      this.scale = Math.max(newScale, 100);
-      const topVal = ev.clientY - this.scale / 2;
-      const leftVal = ev.clientX - this.scale / 2;
+    this.xScale = style.offsetWidth/1600;
+    this.yScale = style.offsetHeight/900;
+  }
+  
+  onMousewheel(event, input) {
+    if (event.ctrlKey == true) {
+      event.preventDefault();
+      this.scalehelper = this.scale - event.deltaY * 0.2;
+      if(this.scalehelper > 400) {
+        return;
+      }
+      this.scale = Math.max(this.scalehelper, 100);
+      const topVal = event.clientY - this.scale / 2;
+      const leftVal = event.clientX - this.scale / 2;
       this.top = topVal >= 0 ? topVal : 0;
       this.left = leftVal >= 0 ? leftVal : 0;
-    });
+    } else if(input) {
+      this.scalehelper = 100 + (+event?.target.value);
+      if(this.scalehelper > 400) {
+        return;
+      }
+      this.scale = Math.max(this.scalehelper, 100);
+      const topVal = event.clientY - this.scale / 2;
+      const leftVal = event.clientX - this.scale / 2;
+      this.top = topVal >= 0 ? topVal : 0;
+      this.left = leftVal >= 0 ? leftVal : 0;
+    }
   }
 }
